17746fe95c1717d2d7a3064ab1ae0bfae06a9855
Mrinaal Nagpal <mrinaal1712@gmail.com>
2025-04-06 19:07:00 -0400

    signup

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { SignIn, SignUp, RedirectToSignIn } from "@clerk/clerk-react";
+import { SignIn, SignUp } from "@clerk/clerk-react";
 import App from "./App.jsx";
 import LandingPage from "./LandingPage.jsx";
 import "./index.css";
 
-
-const clerkFrontendApi = "pk_test_aW1tdW5lLWthbmdhcm9vLTk0LmNsZXJrLmFjY291bnRzLmRldiQ"; 
+const PUBLISHABLE_KEY = "pk_test_aW1tdW5lLWthbmdhcm9vLTk0LmNsZXJrLmFjY291bnRzLmRldiQ";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ClerkProvider frontendApi={clerkFrontendApi}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <Router>
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/match" element={<App />} />
           <Route path="/sign-in" element={<SignIn routing="path" path="/sign-in" />} />
-<Route path="/sign-up" element={<SignUp routing="path" path="/sign-up" />} />
-          {/* Login & Sign Up routes will go here soon */}
+          <Route path="/sign-up" element={<SignUp routing="path" path="/sign-up" />} />
         </Routes>
       </Router>
     </ClerkProvider>
